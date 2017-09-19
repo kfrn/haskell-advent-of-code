0c0b65fc371eeecd1dd579cda9834772de001af1
kfrn <knfrances@gmail.com>
2017-09-19 14:46:29 +1200

    Complete exercise 03a - gives correct answer. Had inverted logic

--- 03/Main.hs
+++ 03/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.List (sort)
 import Data.List.Extra (trim)
 import Data.List.Split (splitOn)
 import System.Environment (getArgs)
@@ -12,12 +13,10 @@ cleanTextTriple :: String -> [String]
 cleanTextTriple inputString = map trim splitString
   where
     splitString = filter (\elem -> length elem > 0) $ splitOn " " inputString
-    -- NOTE: alternative filter FN is: (not . null)
 
--- triplesFromString :: String -> [Maybe Int]
-triplesFromString :: String -> (Maybe Int, Maybe Int, Maybe Int)
+triplesFromString :: String -> (Int, Int, Int)
 triplesFromString inputString =
-  arrayToTriple (map (\elem -> readMaybe elem :: Maybe Int) cleanTuple)
+  arrayToTriple (map (\elem -> read elem :: Int) cleanTuple) -- NOTE: does not handle invalid cases
   where
     cleanTuple = cleanTextTriple inputString
 
@@ -25,7 +24,7 @@ validTriangle :: (Int, Int, Int) -> Bool
 validTriangle (a, b, c) =
   if allSidesSameLength (a, b, c)
     then True
-    else if longestSide (a, b, c) > sumShorterSides (a, b, c)
+    else if sumShorterSides (a, b, c) > longestSide (a, b, c)
            then True
            else False
 
@@ -36,29 +35,22 @@ longestSide :: (Int, Int, Int) -> Int
 longestSide (a, b, c) = max a (max b c)
 
 sumShorterSides :: (Int, Int, Int) -> Int
-sumShorterSides (a, b, c) = smallestNumber (a, b, c) + middleNumber (a, b, c)
+sumShorterSides (a, b, c) = shortestSide (a, b, c) + middleSide (a, b, c)
 
-smallestNumber :: (Int, Int, Int) -> Int
-smallestNumber (a, b, c) = min a (min b c)
+shortestSide :: (Int, Int, Int) -> Int
+shortestSide (a, b, c) = min a (min b c)
 
-middleNumber :: (Int, Int, Int) -> Int
-middleNumber (a, b, c)
-  | twoSidesTheSameLength (a, b, c) = mostFrequentNumber (a, b, c)
-  | otherwise = max a (min b c)
+middleSide :: (Int, Int, Int) -> Int
+middleSide (a, b, c) = sort [a, b, c] !! 1
 
 twoSidesTheSameLength :: (Int, Int, Int) -> Bool
 twoSidesTheSameLength (a, b, c) = a == b || b == c || a == c
-
-mostFrequentNumber :: (Int, Int, Int) -> Int
-mostFrequentNumber (a, b, c)
-  | a == b = a
-  | a == c = a
-  | otherwise = b
 
 main :: IO ()
 main = do
   [filename] <- getArgs
   contents <- readFile filename
   let triples = map triplesFromString (lines contents)
-  print triples
-  print (length triples)
+  -- print triples
+  let validTriangles = filter validTriangle triples
+  print (length validTriangles)
--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 [Advent of Code](https://adventofcode.com/2016/about) is a series of small programming puzzles for a variety of skill levels. They are self-contained and are just as appropriate for an expert who wants to stay sharp as they are for a beginner who is just learning to code. Each puzzle calls upon different skills and has two parts that build on a theme.
 
 <!-- 1. [Day 1](https://adventofcode.com/2016/day/1) -->
-2. [Day 2](https://adventofcode.com/2016/day/2)
+2. [Keypad input](https://adventofcode.com/2016/day/2)
+3. [Possible triangles](http://adventofcode.com/2016/day/3)
