6dbf14c15f018e90e04044e893be786db862c52d
kfrn <knfrances@gmail.com>
2017-09-21 13:16:38 +1200

    Complete exercise 06b

--- 06/Main.hs
+++ 06/Main.hs
@@ -9,14 +9,15 @@ groupLetters inputString = do
   let uniques = nub (sort inputString)
   map (\targetLetter -> filter (== targetLetter) inputString) uniques
 
-mostCommonLetter :: [[Char]] -> [Char]
-mostCommonLetter listOfGroupedLetters = nub (head orderedByFrequency)
-  where orderedByFrequency = sortBy (flip $ comparing length) listOfGroupedLetters
+leastCommonLetter :: [[Char]] -> [Char]
+leastCommonLetter listOfGroupedLetters = nub (head orderedByFrequency)
+  where
+    orderedByFrequency = sortBy (comparing length) listOfGroupedLetters
 
 main :: IO ()
 main = do
   [filename] <- getArgs
   contents <- readFile filename
   let columns = transpose (lines contents)
-  let letters = map mostCommonLetter (map groupLetters columns)
+  let letters = map leastCommonLetter (map groupLetters columns)
   print (concat letters)
