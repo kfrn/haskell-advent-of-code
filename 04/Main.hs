45105dec7e01abeb0134eff0577a7894c764252b
kfrn <knfrances@gmail.com>
2017-09-21 12:33:00 +1200

    Complete exercise 04b

--- 04/Main.hs
+++ 04/Main.hs
@@ -1,14 +1,16 @@
 module Main where
 
 import Data.Char (isLetter)
-import Data.List (nub, sort, sortBy)
+import Data.List (elemIndex, nub, sort, sortBy)
+import Data.Maybe (fromJust)
+import Data.Ord (comparing)
 import System.Environment (getArgs)
 import Text.Regex.PCRE
-import Data.Ord (comparing)
 
 data Room = Room
   { encryptedName :: String
   , sectorID :: Int
+  , decryptedName :: String
   , checksum :: String
   } deriving (Show)
 
@@ -23,41 +25,40 @@ extractEncryptedName roomString =
   filter isLetter (roomString =~ "[a-z\\-]+\\-[0-9]")
 
 makeRoom :: String -> Room
-makeRoom roomString =
+makeRoom roomString = do
+  let sectorID = extractSectorID roomString
+  let encryptedName = extractEncryptedName roomString
   Room
-    (extractEncryptedName roomString)
-    (extractSectorID roomString)
+    encryptedName
+    sectorID
+    (decryptName encryptedName sectorID)
     (extractChecksum roomString)
 
-groupLetters :: String -> [String]
-groupLetters encryptedName = do
-  let uniques = nub (sort encryptedName)
-  map (\targetLetter -> filter (== targetLetter) encryptedName) uniques
+decryptName :: String -> Int -> String
+decryptName encryptedName sectorID =
+  map (\letter -> decryptLetter letter sectorID) encryptedName
 
-orderByFrequency :: [[Char]] -> [[Char]]
-orderByFrequency listOfGroupedLetters = sortBy (flip $ comparing length) listOfGroupedLetters
-
-mostToLeastFrequentLetters :: String -> [[Char]]
-mostToLeastFrequentLetters encryptedName = orderByFrequency groupedLetters
+decryptLetter :: Char -> Int -> Char
+decryptLetter inputLetter sectorID =
+  if newLetterIndex < 26
+    then alphabet !! newLetterIndex
+    else alphabet !! (newLetterIndex - 26)
   where
-    groupedLetters = groupLetters encryptedName
+    newLetterIndex = currentLetterAlphabetIndex inputLetter + offset sectorID
+    -- newLetterIndex = fmap ((+) (offset sectorID)) $ currentLetterAlphabetIndex inputLetter -- partially applied FN returning a functor: Maybe Int.
 
-realRoom :: Room -> Bool
-realRoom (Room encryptedName sectorID checksum) =
-  fiveMostCommonLetters encryptedName == sort checksum
+currentLetterAlphabetIndex :: Char -> Int
+currentLetterAlphabetIndex letter = fromJust (elemIndex letter alphabet)
 
-fiveMostCommonLetters :: String -> [Char]
-fiveMostCommonLetters encryptedName =
-  collapse (sort (take 5 (mostToLeastFrequentLetters encryptedName)))
-  where
-    collapse a = concatMap nub a
+alphabet :: [Char]
+alphabet = ['a' .. 'z']
+
+offset :: Int -> Int
+offset sectorID = sectorID `mod` (length alphabet)
 
 main :: IO ()
 main = do
   [filename] <- getArgs
   contents <- readFile filename
   let rooms = map makeRoom (lines contents)
-  let realRooms = filter realRoom rooms
-  let realRoomSectorIDs = map sectorID realRooms
-  let sumSectorIDs = sum realRoomSectorIDs
-  print sumSectorIDs
+  mapM_ print rooms
