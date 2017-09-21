0b4fd97ba67eb74d3d068c47f983b015bbdf4d07
kfrn <knfrances@gmail.com>
2017-09-21 12:41:25 +1200

    Refactor 04b - use infinite list

--- 04/Main.hs
+++ 04/Main.hs
@@ -39,22 +39,15 @@ decryptName encryptedName sectorID =
   map (\letter -> decryptLetter letter sectorID) encryptedName
 
 decryptLetter :: Char -> Int -> Char
-decryptLetter inputLetter sectorID =
-  if newLetterIndex < 26
-    then alphabet !! newLetterIndex
-    else alphabet !! (newLetterIndex - 26)
+decryptLetter inputLetter sectorID = cycle alphabet !! newLetterIndex
   where
-    newLetterIndex = currentLetterAlphabetIndex inputLetter + offset sectorID
-    -- newLetterIndex = fmap ((+) (offset sectorID)) $ currentLetterAlphabetIndex inputLetter -- partially applied FN returning a functor: Maybe Int.
+    newLetterIndex = currentLetterAlphabetIndex inputLetter + sectorID
 
 currentLetterAlphabetIndex :: Char -> Int
 currentLetterAlphabetIndex letter = fromJust (elemIndex letter alphabet)
 
 alphabet :: [Char]
 alphabet = ['a' .. 'z']
-
-offset :: Int -> Int
-offset sectorID = sectorID `mod` (length alphabet)
 
 main :: IO ()
 main = do
